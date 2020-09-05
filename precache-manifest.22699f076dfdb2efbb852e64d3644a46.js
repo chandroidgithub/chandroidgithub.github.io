20723c1ae15d3bf5985d4d1f5b9222272e7a679d
CHANDAN MANNA <chandan.alltime4u@gmail.com>
2020-09-05 18:00:27 +0530

    First_upload

---
+++ precache-manifest.22699f076dfdb2efbb852e64d3644a46.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb5453a5711812e456e4d1fa97ad287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa52b76623c2e9a3a49b",
+    "url": "/static/css/2.b9312890.chunk.css"
+  },
+  {
+    "revision": "5fbbb938d987c47f4680",
+    "url": "/static/css/main.34d8e199.chunk.css"
+  },
+  {
+    "revision": "fa52b76623c2e9a3a49b",
+    "url": "/static/js/2.69104ff2.chunk.js"
+  },
+  {
+    "revision": "699360320838612aed2fe6e2b592b376",
+    "url": "/static/js/2.69104ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbbb938d987c47f4680",
+    "url": "/static/js/main.25648a24.chunk.js"
+  },
+  {
+    "revision": "8de2bd8f6287307f0df8",
+    "url": "/static/js/runtime-main.c22c8184.js"
+  },
+  {
+    "revision": "947f8bfdd01a86745919ca6f4316e167",
+    "url": "/static/media/ReviewImages.947f8bfd.jpg"
+  },
+  {
+    "revision": "6a07f6df104f6af3374bbb8d7f45c00c",
+    "url": "/static/media/SticPicsLogo.6a07f6df.gif"
+  },
+  {
+    "revision": "7048354642abeaf7b2e60f0cabd0c43f",
+    "url": "/static/media/addressIcon.70483546.svg"
+  },
+  {
+    "revision": "0232f87f0b86877a24dd2b454017781a",
+    "url": "/static/media/boldIcon.0232f87f.svg"
+  },
+  {
+    "revision": "e878b3bfea41cb0ac7d16177a1c0ddbd",
+    "url": "/static/media/classicIcon.e878b3bf.svg"
+  },
+  {
+    "revision": "a5576cf9d644282d71591544fe033189",
+    "url": "/static/media/cleanIcon.a5576cf9.svg"
+  },
+  {
+    "revision": "c8e13f53b8d92d8f49afff01f89f5100",
+    "url": "/static/media/everIcon.c8e13f53.svg"
+  },
+  {
+    "revision": "c1e769605e4686003967900cb433dc65",
+    "url": "/static/media/facebook.c1e76960.svg"
+  },
+  {
+    "revision": "fb36c901b08f2089b331d3d87ad1f8a6",
+    "url": "/static/media/frame2.fb36c901.mp4"
+  },
+  {
+    "revision": "44242cbad5b6a244ccc89c71b886c8da",
+    "url": "/static/media/gallery Wall-B&W.44242cba.jpeg"
+  },
+  {
+    "revision": "7fea10b7f9da29a8b8125fc81ec69ea9",
+    "url": "/static/media/googleDrive.7fea10b7.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "71c285a1827edc45c6e9d6e7939798b7",
+    "url": "/static/media/play.71c285a1.svg"
+  },
+  {
+    "revision": "a9c05ac3593fa5181a5da85310fc0834",
+    "url": "/static/media/upload-icon.a9c05ac3.svg"
+  },
+  {
+    "revision": "6de7e0a988aac69de62a12f0b8f7c98c",
+    "url": "/static/media/wallImage.6de7e0a9.jpg"
+  }
+]);
